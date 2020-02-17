088f79642bedae01c0f081e8f9c3402620d60ba5
InitialMe <37935956+InitialMe@users.noreply.github.com>
2020-02-17 17:05:23 +0800

    Update 在用户目录创建文件夹.py

--- 在用户目录创建文件夹.py
+++ 在用户目录创建文件夹.py
@@ -5,27 +5,27 @@ step4 ：否则则打印已存在
 
 '''
 
-'''
-import os
 
-Message = "这个文件已经存在"
+#import os
 
-UserPath = os.path.expanduser('~')
-makedir = input()
-if not os.path.exists(os.path.join(UserPath,makedir)):
-    os.mkdir(os.path.join(UserPath,makedir))
-else:
-    print(Message)
-'''
+#Message = "这个文件已经存在"
 
-'''
-目的：在用户目录创建不存在的文件夹
-step1 : 获取并打印出当前用户目录  os.path.expanduser
-step2 ：输入目录名判断是否有相同目录  os.path.exists  os.path.join(home, TESTDIR)
-step3 ：没有则创建该目录        os.makedirs
-step4 ：否则则打印已存在
+#UserPath = os.path.expanduser('~')
+#makedir = input()
+#if not os.path.exists(os.path.join(UserPath,makedir)):
+ #   os.mkdir(os.path.join(UserPath,makedir))
+#else:
+ #   print(Message)
 
-'''
+
+
+# 目的：在用户目录创建不存在的文件夹
+# step1 : 获取并打印出当前用户目录  os.path.expanduser
+# step2 ：输入目录名判断是否有相同目录  os.path.exists  os.path.join(home, TESTDIR)
+# step3 ：没有则创建该目录        os.makedirs
+# step4 ：否则则打印已存在
+
+
 
 import os
 import argparse
